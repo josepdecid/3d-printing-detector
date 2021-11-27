c70729994029ef1e2f73eb6d606af3463bc9f326
Josep de Cid <josep.de.cid@gmail.com>
2021-11-27 23:38:29 +0100

    Merge evaluator with API

--- src/api/app.py
+++ src/api/app.py
@@ -1,13 +1,16 @@
-import base64
 import argparse
+import base64
+import os
 from io import BytesIO
 
 import numpy as np
 from PIL.Image import Image
 from flask import Flask, request, Response
 
+from eval import Evaluator
 
-def create_app():
+
+def create_app(checkpoint_path: str, device: str) -> Flask:
     app = Flask(__name__, instance_relative_config=True)
 
     @app.route('/process_image', methods=['POST'])
@@ -20,20 +23,30 @@ def create_app():
         img_data_decoded = base64.b64decode(img_b64_data)
         img_array = np.array(Image.open(BytesIO(img_data_decoded)))
 
+        checkpoints_folder = os.environ.get('CHECKPOINTS_PATH')
+        checkpoint_full_path = os.path.join(checkpoints_folder, checkpoint_path)
 
+        evaluator = Evaluator(checkpoint_full_path, device)
+        class_name = evaluator.evaluate(img_array)
+
+        return Response(class_name, status=200)
 
     return app
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('checkpoint', type=str)
     parser.add_argument('--host', default='127.0.0.1', type=str)
     parser.add_argument('--port', default='8080', type=str)
+    parser.add_argument('--gpu', default=False, type=bool)
     parser.add_argument('--dev', default=False, type=bool,
                         help='Run the app in development (debug) mode. False by default.')
     args = parser.parse_args()
 
-    app = create_app()
+    device = 'gpu' if args.gpu else 'cpu'
+
+    app = create_app(args.checkpoint, device)
     app.run(host=args.host, port=args.port, debug=args.dev_mode)
 
 
--- src/eval.py
+++ src/eval.py
@@ -1,31 +1,29 @@
 import os
-from typing import Union, Optional
+from typing import Optional
 
 import hydra
 import numpy as np
 import torch
 from PIL import Image
 from omegaconf import DictConfig
-from torchvision.transforms import Compose, ToTensor, Normalize
+from torchvision.transforms import ToTensor, Compose, Normalize
 
 from datasets.Piece3DPrintDataset import Piece3DPrintDataset
-
-eval_transformations = Compose([
-    ToTensor(),
-    Normalize(
-        mean=(0.5, 0.5, 0.5),
-        std=(0.5, 0.5, 0.5)
-    )
-])
 
 
 class Evaluator:
     def __init__(self, checkpoint_path: Optional[str], device: str):
         # TODO: Load proper model instead of an empty one
         self.__model = torch.nn.Module()
-
         if checkpoint_path is not None:
-            Evaluator.__load_checkpoint(self.__model, checkpoint_path)
+            self.__model = Evaluator.__load_checkpoint(self.__model, checkpoint_path)
+
+        # Simple eval transformations with only tensorized and normalized data
+        self.__eval_transformations = Compose([
+            ToTensor(),
+            Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        ])
+        self.__dataset = Piece3DPrintDataset(os.environ.get('DATASET_PATH'), self.__eval_transformations)
 
         # Set the model to the specified device
         self.__device = torch.device(device)
@@ -33,20 +31,18 @@ class Evaluator:
         # Set in evaluation model to avoid unexpected random variations
         self.__model.eval()
 
-    def evaluate(self, img: Union[np.ndarray, torch.tensor]):
-        if isinstance(img, np.ndarray):
-            img = torch.from_numpy(img)
-
+    def evaluate(self, img: np.ndarray) -> str:
         # Transform the image with the evaluation transformations
-        img = eval_transformations(img)
+        img = self.__eval_transformations(img)
         # Add a new dimension for the batches with a single instance
         img = torch.unsqueeze(img, 0)
         # Send the instance to evaluate to the same device as the model
         img = img.to(self.__device)
 
         with torch.no_grad():
-            prediction = self.__model(img)
-            return prediction
+            predicted_class_idx = self.__model(img)
+
+        return self.__dataset.class_from_idx[predicted_class_idx]
 
     @staticmethod
     def __load_checkpoint(model: torch.nn.Module, checkpoint_path: str) -> torch.nn.Module:
@@ -62,10 +58,7 @@ def main(cfg: DictConfig) -> None:
     img = np.array(Image.open(cfg.image))
 
     evaluator = Evaluator(checkpoint_path, cfg.device)
-    predicted_class_idx = evaluator.evaluate(img)
-
-    dataset = Piece3DPrintDataset(os.environ.get('DATASET_PATH'), eval_transformations)
-    class_name = dataset.class_from_idx[predicted_class_idx]
+    class_name = evaluator.evaluate(img)
 
     print(class_name)
 
